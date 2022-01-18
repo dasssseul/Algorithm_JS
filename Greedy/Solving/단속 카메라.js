43b575b1b261e055e744e9228717f90f5b1f785a
dasssseul <daseul8086@gmail.com>
2022-01-19 00:36:22 +0900

    feat : 그리디 단속 카메라 풀이

---
+++ Greedy/Solving/단속 카메라.js
@@ -0,0 +1,12 @@
+function solution(routes) {
+  let answer = 1;
+  routes.sort((a, b) => a[1] - b[1]);
+  let camera = routes[0][1];
+  for (let i = 1; i < routes.length; i++) {
+    if (routes[i][0] > camera) {
+      answer++;
+      camera = routes[i][1];
+    }
+  }
+  return answer;
+}
