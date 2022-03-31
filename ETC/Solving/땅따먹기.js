e7d2a3fe9c8578cd7f691ccc3df978f22d8acc0f
dasssseul <daseul8086@gmail.com>
2022-03-31 23:56:16 +0900

    feat : 땅따먹기 풀이

---
+++ ETC/Solving/땅따먹기.js
@@ -0,0 +1,34 @@
+function solution(land) {
+  for (let i = 1; i < land.length; i++) {
+    land[i][0] += Math.max(land[i - 1][1], land[i - 1][2], land[i - 1][3]);
+    land[i][1] += Math.max(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
+    land[i][2] += Math.max(land[i - 1][1], land[i - 1][0], land[i - 1][3]);
+    land[i][3] += Math.max(land[i - 1][1], land[i - 1][2], land[i - 1][0]);
+  }
+  return Math.max(...land[land.length - 1]);
+}
+
+console.log(
+  solution([
+    [1, 2, 3, 5],
+    [5, 6, 7, 8],
+    [4, 3, 2, 1],
+  ])
+);
+
+// reduce 활용
+function solution(land) {
+  return Math.max(
+    ...land.reduce(
+      (a, c) => {
+        return [
+          c[0] + Math.max(a[1], a[2], a[3]),
+          c[1] + Math.max(a[0], a[2], a[3]),
+          c[2] + Math.max(a[0], a[1], a[3]),
+          c[3] + Math.max(a[0], a[1], a[2]),
+        ];
+      },
+      [0, 0, 0, 0]
+    )
+  );
+}
