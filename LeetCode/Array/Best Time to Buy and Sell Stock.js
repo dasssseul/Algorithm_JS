e669a21c49df93d142d6df15c2def1e034082578
dasssseul <daseul8086@gmail.com>
2022-04-02 16:28:42 +0900

    feat : 문제별 풀이 추가

--- LeetCode/Array/Best Time to Buy and Sell Stock.js
+++ LeetCode/Array/Best Time to Buy and Sell Stock.js
@@ -1,3 +1,9 @@
+// 접근 방식
+// 현재 인덱스의 수와 다음 인덱스의 수를 비교
+// 1. 다음 인덱스의 수가 작을 경우 minPrice와 비교해 더 작은 수로 업데이트
+// 2. 다음 인덱스의 수가 클 경우 다음 인덱스의 수 - minPrice 와 answer을 비교해 더 큰 수로 업데이트
+// 3. 끝까지 반복 후 answer 리턴
+
 function solution(prices) {
   let answer = 0;
   let minPrice = prices[0];
--- LeetCode/Array/Two Sum.js
+++ LeetCode/Array/Two Sum.js
@@ -1,3 +1,7 @@
+// 접근 방식
+// 이중 for문을 돌면서 두 수를 더한 수가 target과 같을 경우
+// 두 수의 인덱스 값 반환
+
 function solution(nums, target) {
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
